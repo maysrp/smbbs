d18fb51a61bb68ed3a03cb4f6b12e93e574de166
egist <maysrp@gmail.com>
2016-09-03 22:16:19 +0800

    user index 分页

--- App/Lib/Action/UserAction.class.php
+++ App/Lib/Action/UserAction.class.php
@@ -18,7 +18,19 @@
 				return;
 			}
 			$post=D('Post')->where("uid='".$uid."'")->select();
-			$this->assign("post",$post);
+			$count=count($post);
+			import('ORG.Util.Page');
+			$Page=new Page($count,10);
+			$Page->setConfig('header',"个主题");
+			if($_GET['p']<1){
+				$_GET['p']=1;
+			}else{
+				$_GET['p']=(int)$_GET['p'];
+			}
+			$list=array_slice($post,10*($_GET['p']-1),10);
+			$show=$Page->show();
+			$this->assign("page",$show);
+			$this->assign("post",$list);
 			$this->assign("info",$info);
 			$this->display();
 
