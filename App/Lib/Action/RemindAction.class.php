25effb1088251f18b59324c9cc5ac3e68a395dbf
egist <maysrp@gmail.com>
2016-09-04 16:05:08 +0800

    消息提醒

--- App/Lib/Action/RemindAction.class.php
+++ App/Lib/Action/RemindAction.class.php
@@ -8,12 +8,12 @@
 			if ($_GET['p']<1) {
 				$_GET['p']=1;
 			}else{
-				$_GET=(int)$_GET['p']/10;
+				$_GET['p']=(int)$_GET['p'];
 			}
 			$Page->setConfig("header","条消息");
-			$list=array_alice($all,10*($_GET['p']-1),10);
+			$list=array_slice($all,10*($_GET['p']-1),10);
 			$show=$Page->show();
-			$this->assign("page",$page);
+			$this->assign("page",$show);
 			$this->assign("remind",$list);
 			$this->display();
 		}
@@ -35,8 +35,20 @@
 			$this->success("全部设置为已读");
 		}
     	function isread(){
-        	$isread=D('Remind')->isread();
-        	$this->assign("remind",$isread);
+        	$all=D('Remind')->isread();
+        	$count=count($all);
+			import("ORG.Util.Page");
+			$Page=new Page($count,10);
+			if ($_GET['p']<1) {
+				$_GET['p']=1;
+			}else{
+				$_GET['p']=(int)$_GET['p'];
+			}
+			$Page->setConfig("header","条消息");
+			$list=array_slice($all,10*($_GET['p']-1),10);
+			$show=$Page->show();
+			$this->assign("page",$show);
+        	$this->assign("remind",$list);
         	$this->display();
     	}		
 	}
