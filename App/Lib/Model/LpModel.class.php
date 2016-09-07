2a80aa468adf4faff47abd074cc1acf01aeb5828
egist <maysrp@gmail.com>
2016-09-08 02:03:33 +0800

    积分&&等级 #2

--- App/Lib/Model/LpModel.class.php
+++ App/Lib/Model/LpModel.class.php
@@ -1,6 +1,6 @@
 <?php
 	class LpModel extends Model{
-		//point level 同时获得
+		//point level 同时获得;type 0 为减 1为加
 		function lp($num){
 			$uid=$_SESSION['uid'];
 			if($uid){
@@ -37,11 +37,21 @@
 		function level($level,$uid){
 			if($level){
 				M('User')->where("uid='".$uid."'")->setInc("level",$level);
+				$add['uid']=$uid;
+				$add['time']=time();
+				$add['level']=$level;
+				$add['type']=1;
+				$this->add($add);
 			}
 		}
 		function level_dec($level,$uid){
 			if($level){
 				M('User')->where("uid='".$uid."'")->setDec("level",$level);
+				$add['uid']=$uid;
+				$add['time']=time();
+				$add['level']=$level;
+				$add['type']=0;
+				$this->add($add);
 			}
 		}
 
@@ -50,12 +60,22 @@
 		function point($point,$uid){
 			if($point){
 				M('User')->where("uid='".$uid."'")->setInc("point",$point);
+				$add['uid']=$uid;
+				$add['time']=time();
+				$add['point']=$point;
+				$add['type']=1;
+				$this->add($add);
 			}
 
 		}
 		function point_dec($point,$uid){
 			if($point){
 				M('User')->where("uid='".$uid."'")->setDec("point",$point);
+				$add['uid']=$uid;
+				$add['time']=time();
+				$add['point']=$point;
+				$add['type']=0;
+				$this->add($add);
 			}
 
 		}
